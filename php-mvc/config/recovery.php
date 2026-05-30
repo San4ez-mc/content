64e6d047aa74ec9e18460185bfd248db877fd4c9
San4ez-mc <olexandrmatsuk@gmail.com>
2026-05-30 03:13:30 +0300

    security: untrack config/mail.php + recovery.php (секрети), додано .example + .gitignore

--- php-mvc/config/recovery.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Recovery code for password reset (no email needed)
-// Change this hash if you want to use a different recovery code
-// Current code: FINEKO-RESET-2026
-// Generate new hash: php -r "echo password_hash('YOUR-CODE', PASSWORD_DEFAULT);"
-return [
-    'recovery_code_hash' => '$2y$10$wTS/uiasW9cxNF3x1P1lC.aSpguix7rSs4AsqHSkHZCDxWZ3UMySS',
-];
